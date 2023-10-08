f7862c7173111b3276cc63d09165b14f7eb7dd0a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-07 18:47:47 -0700

    C

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-        int i;
+    int i;
 
-        for (i = 48; i < 58; i++)
-        {
-                putchar(i);
-                if (i != 57)
-                {
-                      putchar(',');
-                      putchar(' ');
-                }
+    for (i = 48; i < 58; i++)
+    {
+    putchar(i);
+    if (i != 57)
+    {
+    putchar(',');
+    putchar(' ');
+    }
 
-        }
-        putchar('\n');
-        return(0);
+    }
+    putchar('\n');
+    return(0);
 }
