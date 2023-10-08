f7088b0df11b35d055f8729ec2cd22e3d408a639
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-07 17:56:38 -0700

    C

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,14 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int i;
+int main() {
+    int i;
+    for(i = 0; i < 10; i++)
+    {
+        printf("%d", i);
+    }
+    putchar('\n');
 
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0)}
-
+    return 0;
+}
