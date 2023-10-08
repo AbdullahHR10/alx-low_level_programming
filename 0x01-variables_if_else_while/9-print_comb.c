54dafada06fed9d349ebec8e77dec44041556f73
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-07 18:50:24 -0700

    C

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,12 +4,6 @@
  * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
-#include <stdio.h>
-
-/**
- * main - prints all possible combinations of single-digit numbers
- * Return: Always 0 (Success)
- */
 int main(void)
 {
 	int n;
