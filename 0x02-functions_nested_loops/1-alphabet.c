a7a058fa9672350cfe10a5e23f65d58e471b99a4
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-08 20:22:36 -0700

    Prints lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+* main : Prints the alphabet in lowercase followed by a new line
+*
+* Return : Always 0
+*/
+int main() {
+    char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+    int i;
+    for (i = 0; i < 26; i++)
+    {
+        putchar(alphabet[i]);
+    }
+    putchar('\n');
+    return(0);
+}
