5597202a42ce364e297a68841f5425bc5fd9467e
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-09 16:34:01 -0700

    Prints_putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,15 @@
 #include "main.h"
 
-/*
-* main : Prints "_putchar" followed by a new line
-*
-* Return : Always 0
-*/
-int main() {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-
-    return 0;
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return(0);
 }
