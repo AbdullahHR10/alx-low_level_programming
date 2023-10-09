9ea36f62424782a3f277795efc5091f1fe5815f3
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-09 16:55:59 -0700

    Prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
+	_putchar('_');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
@@ -17,5 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
