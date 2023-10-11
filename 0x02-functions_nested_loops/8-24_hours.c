01b49e60424379b86c884eee028192f9f6404892
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-11 00:38:14 -0700

    test

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ *
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	if (m < 60, m++)
+	putchar(m);
+	putchar('\n');
+
+	if (h < 24, h++)
+	putchar(h);
+	putchar('\n')
+}
