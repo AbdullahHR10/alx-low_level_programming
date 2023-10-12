d9bf2059f92b9a3b0e41bc208591861737254683
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-11 23:11:14 -0700

    Main header file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if uppercase, 0 if lowercase
+ */
+
+int _isupper(int c)
+{
+	for (c < 'A'; c > 'Z'; c++)
+    {
+    return (1);
+    }
+    else
+    {
+    return (0);
+    }
+}
