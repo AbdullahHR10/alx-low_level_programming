4fb2fdd9e522953e0e1c7165588b631771949861
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-20 01:07:13 -0700

    prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,12 @@ void more_numbers(void)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			if ( c > 10)
-			{
-				_putchar((c) + '0');
-			}
-			if ( c <= 10)
+			if ( c >= 10)
 			{
 				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
 			}
+			_putchar((c % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
