1a9e653789299868287a6ba0d02ffb2071fd8728
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-25 01:20:02 -0700

    Prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,6 +2,7 @@
 
 /**
  * _puts - prints a string to stdout
+ * @str: string to be printed
  * Return: Always 0
  */
 
@@ -11,7 +12,7 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0'
 	{
 		_putchar(str[i]);
 		i++;
