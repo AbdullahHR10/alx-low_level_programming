536f70c991aa26a1d31be8ffaba25a42c266adb9
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-27 06:55:30 -0700

    udate

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
- * @str: string to be printed
- * Return: Always 0
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
  */
-
-
 
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		_putchar(str[i]);
 		i++;
