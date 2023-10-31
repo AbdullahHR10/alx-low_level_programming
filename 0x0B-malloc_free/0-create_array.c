46f87dcc4acfebd1a89e101c9c880a0bc7da0c4f
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-31 07:13:32 -0700

    Main header file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - reates an array of chars, and initializes it with a char
+ * @size: int
+ * @c: char
+ * Return: NULL if size = 0
+ */
