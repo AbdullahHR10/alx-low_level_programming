b7d29073204523570f2808d28fa618e2736f7691
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-10-31 09:47:29 -0700

    Returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,31 +1,31 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _strdup - Returns a pointer to a newly allocated space in memory
- * @str: char
- * Return: NULL if str = NULL, otherwise a pointer
+ * _strdup - Entry point
+ *@str: string we need to duplicate
+ * Return: a pointer to the duplicated string or NULL
  */
-
-
 char *_strdup(char *str)
 {
-	int i;
-	char *a;
+	char *strnew = NULL;
+	unsigned int i;
+	int n;
 
 	if (str == NULL)
+		return (NULL);
+	for (n = 0; str[n] != '\0'; n++)
+		;
+	strnew = (char *)malloc(n + 1 * sizeof(char));
+	if (strnew != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			strnew[i] = str[i];
+	} else
 	{
 		return (NULL);
 	}
-	a = malloc(sizeof(char));
-	if (a == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		a[i] = str[i];
-	}
-	return (a);
+	strnew[i] = '\0';
+	return (strnew);
 }
