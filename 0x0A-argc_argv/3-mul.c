3851a663372e08844941c0815e450b9d3a2539f8
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-01 08:48:43 -0700

    Multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,20 +11,22 @@
 
 int main(int argc, char *argv[])
 {
+    int num1, num2, r;
+
     if (argc < 3) {
         printf("Error: Not enough arguments\n");
         return 1;
     }
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
 
     if (num1 == 0 || num2 == 0) {
         printf("Error: Invalid input\n");
         return 1;
     }
 
-    int r = num1 * num2;
+    r = num1 * num2;
     printf("%d\n", r);
 
     return 0;
