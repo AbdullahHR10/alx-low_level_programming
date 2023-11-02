b846a4ee024588eca2e1f264821707e4f879ba91
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-01 21:27:38 -0700

    Returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,28 +11,43 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i, j;
-	int **arr;
+    int i, j;
+    int **arr;
 
-	if (width < 0 || height < 0)
-	{
-		return (NULL);
-	}
-	arr = (int **)malloc(height * sizeof(int));
-	for (i = 0; i < height; i++)
-	{
-		arr[i] = (int *)malloc(width * sizeof(int));
-	}
-	if (arr[i] == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			arr[i][j] = 0;
-		}
-	}
-	return (arr);
+    if (width < 0 || height < 0)
+    {
+        return NULL;
+    }
+
+    // Allocate memory for an array of pointers to rows
+    arr = (int **)malloc(height * sizeof(int *));
+    if (arr == NULL)
+    {
+        return NULL;
+    }
+
+    for (i = 0; i < height; i++)
+    {
+        // Allocate memory for each row
+        arr[i] = (int *)malloc(width * sizeof(int));
+        if (arr[i] == NULL)
+        {
+            // Handle allocation failure, free previously allocated memory, and return NULL
+            for (j = 0; j < i; j++)
+            {
+                free(arr[j]);
+            }
+            free(arr);
+            return NULL;
+        }
+
+        // Initialize the elements to 0
+        for (j = 0; j < width; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+
+    return (arr);
 }
+
