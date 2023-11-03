08936617a1081d761903c1485f0926df666c085b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-03 00:42:46 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,23 @@ int main(void)
 	{
 		if (c % 3 == 0 && c % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (c % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (c % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
 			printf("%d", c);
+		}
+		if (c < 100)
+		{
+			prinf(" ");
 		}
 	}
 	return (0);
