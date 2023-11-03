57b4099b724216e730dbce2fdd555f4562fcfe33
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-03 06:47:41 -0700

    Prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: char
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i, c, j;
+
+	c = _strlen(str);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i];
+		if (str[i + 1] != '\0')
+		i++;
+	}
+	_putchar('\n');
+}
