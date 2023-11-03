b14c55aa2851017318d3917c132abc016dc9c39f
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-03 05:15:37 -0700

    Prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,14 +14,16 @@ void print_triangle(int size)
 	{
 		_putchar('\n');
 	}
-	else {
 	for (i = 0; i < size; i++)
 	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			_putchar(' ');
+		}
 		for (j = 0; j <= i; j++)
 		{
 			_putchar('#');
 		}
 		_putchar('\n');
-	}
 	}
 }
