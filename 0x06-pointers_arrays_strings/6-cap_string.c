75b206aff887153415ddaf298d361ccd498b910c
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-04 10:22:52 -0700

    Capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,8 +17,10 @@ char *cap_string(char *str)
 		if (str[i] > 'a' && str[i] < 'z')
 		{
 			if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-					str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
-					str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+					str[i - 1] == ',' || str[i - 1] == ';' ||
+					str[i - 1] == '.' || str[i - 1] == '!' ||
+					str[i - 1] == '?' || str[i - 1] == '"' ||
+					str[i - 1] == '(' || str[i - 1] == ')' ||
 					str[i - 1] == '{' || str[i - 1] == '}') {
 				str[i] = str[i] - 'a' + 'A';
 			}
