8949bba522d51e3bc8300398059e2a7d9b0743b8
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-09 11:12:57 -0800

    Function selector

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,22 +9,24 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+	int i;
 
-	while (i < 10)
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+
+	i = 0;
+	while (ops[i].op != NULL)
 	{
-		if (s[0] == ops->op[i])
-			break;
+		if (*s == *(ops[i].op) && s[1] == '\0')
+			return (ops[i].f);
 		i++;
 	}
 
-	return (ops[i / 2].f);
+	return (NULL);
 }
