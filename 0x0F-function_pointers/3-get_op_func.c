29bf734b7b2b844a11ce820effd651ab44da540a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-09 10:25:27 -0800

    Function selector

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - Function Selector
+ * @s: char
+ * Return: pointer to function
+ */
+
+int (*get_op_func(char *S))(int, int)
+{
+	int i;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	i = 0;
+	while (ops[i].op)
+	{
+		of(*(ops[i].op) == *s && s[1] == '\0')
+			return ops[i].f;
+			i++;
+	}
+	printf("ERROR\n");
+
+	exit(99);
+}
