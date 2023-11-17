1db0d899296bfac4ab6683e4f06824fe771e6786
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-16 23:49:12 -0800

    Fixed the code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,14 +12,14 @@ int print_last_digit(int i)
 
 	if (i > 0)
 	{
-		c = ((i % 10) + '0');
-		_putchar(c);
+		c = i % 10;
+		_putchar(c + '0');
 	}
 	else if (i < 0)
 	{
-		i = i - 1;
-		c = ((i % 10) + '0');
-		_putchar(c);
+		i = i * -1;
+		c = i % 10
+		_putchar(c + '0');
 	}
 	else
 	{
