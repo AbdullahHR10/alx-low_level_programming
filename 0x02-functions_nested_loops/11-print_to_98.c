943261dae768db04cf6b390e9651f39a49f8a51a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-18 04:20:49 -0800

    Fixed the code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98
@@ -8,25 +9,33 @@
 
 void print_to_98(int n)
 {
-	int i;
-	for (i = n; i <= 98; i++)
-	{
-		if (i < 10)
-		{
-			_putchar('0' + i);
-			_putchar(',');
-			_putchar(' ');
-		}
-		if (i >= 10)
-		{
-			_putchar('0' + (i / 10));
-			_putchar('0' + (i % 10));
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	_putchar('\n');
+    int i;
+
+    if (n <= 98)
+    {
+        for (i = n; i <= 98; i++)
+        {
+            printf("%d", i);
+            if (i != 98)
+            printf(", ");
+        }
+    }
+    if (n > 98)
+    {
+        for (i = n; i >= 98; i--)
+        {
+            printf("%d", i);
+            if (i != 98)
+            printf(", ");
+        }
+    }
+    if (n < 0)
+    {
+        for (i = n; i <= 98; i++)
+        {
+            printf("%d", i);
+            if (i != 98)
+            printf(", ");
+        }
+    }
 }
