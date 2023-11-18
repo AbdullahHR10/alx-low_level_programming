9d2d497006f93f88ec6ea82ff28d1e362dfcd27b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-18 05:09:45 -0800

    Fixed betty style

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,34 +9,34 @@
 
 void print_to_98(int n)
 {
-    int i;
-
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            printf("%d", i);
-            if (i != 98)
-            printf(", ");
-        }
-    }
-    if (n > 98)
-    {
-        for (i = n; i >= 98; i--)
-        {
-            printf("%d", i);
-            if (i != 98)
-            printf(", ");
-        }
-    }
-    if (n < 0)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            printf("%d", i);
-            if (i != 98)
-            printf(", ");
-        }
-    }
-    printf("\n");
+	int i;
+	
+	else if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
 }
