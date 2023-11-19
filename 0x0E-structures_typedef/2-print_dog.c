e75e984094910a480d3cd0281b38103076ddbc0b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-19 09:21:18 -0800

    Added new lines

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,15 +12,15 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 		{
-			printf("Name : (nil)");
+			printf("Name : (nil)\n");
 		}
 		if (d->owner == NULL)
 		{
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		}
 		if (d->age == 0)
 		{
-			printf("Age : (nil)");
+			printf("Age : (nil)\n");
 		}
 		else
 		{
