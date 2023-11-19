6ecea50e4b11d04bbb791f03156cd2497c5e7782
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-19 09:52:31 -0800

    Wrote the code

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: Nothing
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
