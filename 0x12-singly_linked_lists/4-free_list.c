ead2a5cf24feb22398407961365e3d1d6a3163c2
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-20 06:02:10 -0800

    Fixed the code

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,16 +8,13 @@
 
 void free_list(list_t *head)
 {
-	if (head != NULL)
+	while (head != NULL)
 	{
 		if (head->str != NULL)
 		{
 			free(head->str);
 		}
-		if (head->next != NULL)
-		{
-			free(head->next);
-		}
-	free(head);
+	head = head->next;
 	}
+	free(head);
 }
