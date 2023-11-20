d51727aee2ad2e634a7da96edb306f6c69a55fd7
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-20 05:31:42 -0800

    Fixed the code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ size_t print_list(const list_t *h)
 {
 	int num = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
 	if (h->str == NULL)
 	{
@@ -23,7 +23,6 @@ size_t print_list(const list_t *h)
 		num++;
 	}
 	h = h->next;
-	if (h != NULL)
 	{
 	if (h->str == NULL)
 	{
