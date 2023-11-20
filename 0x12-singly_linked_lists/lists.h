d21baafb08ea58df661f2206248b693455d40870
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-20 05:05:50 -0800

    Added endif to header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LISTS_HEADER
+#define LISTS_HEADER
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,4 +24,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
+#endif
