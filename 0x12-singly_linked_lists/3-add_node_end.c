33a24c3ae0326f9319f370d37ce56f14e8329488
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-22 07:11:49 -0800

    Fixed betty style

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,21 +10,21 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-        list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node = malloc(sizeof(list_t));
 	list_t *current;
-
-        if (new_node == NULL)
-        {
-                return (NULL);
-        }
-        new_node->str = strdup(str);
-        if (new_node->str == NULL)
-        {
-                free(new_node);
-                return (NULL);
-        }
-        new_node->len = strlen(str);
-        new_node->next = NULL;
+	
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new_node;
@@ -35,7 +35,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		while (current->next != NULL)
 		current = current->next;
 
-        current->next = new_node;
-    }
-        return (new_node);
+		current->next = new_node;
+	}
+	return (new_node);
 }
