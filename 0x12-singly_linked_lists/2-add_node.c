791ea0749c59c3de481ceb5ffb4b50c4d224a5eb
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-22 06:30:25 -0800

    Test

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,24 +10,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    // Allocate memory for the new node
     list_t *new_node = malloc(sizeof(list_t));
     if (new_node == NULL) {
-        return NULL; // Memory allocation failed
-    }
-
-    // Duplicate the string
-    new_node->str = strdup(str);
-    if (new_node->str == NULL) {
-        free(new_node); // Memory allocation for the string failed
         return NULL;
     }
-
-    // Set values for the new node
+    new_node->str = strdup(str);
+    if (new_node->str == NULL) {
+        free(new_node);
+        return NULL;
+    }
     new_node->len = strlen(str);
     new_node->next = *head;
 
-    // Update the head of the list
     *head = new_node;
 
     return new_node;
