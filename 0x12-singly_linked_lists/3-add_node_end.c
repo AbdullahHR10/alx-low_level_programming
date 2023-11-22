4e2c7dd9c75eebe7327ea034ea11ae16856cadaf
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-22 07:05:04 -0800

    Fixed the code

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,6 @@ list_t *add_node_end(list_t **head, const char *str)
         {
                 return (NULL);
         }
-	*head = new_node;
         new_node->str = strdup(str);
         if (new_node->str == NULL)
         {
@@ -25,6 +24,9 @@ list_t *add_node_end(list_t **head, const char *str)
         }
         new_node->len = strlen(str);
         new_node->next = NULL;
-
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
         return (new_node);
 }
