8e57adc38b8bd6b7be6b1f7be3df6428e11221f1
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-27 09:29:07 -0800

    Wrote the code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,10 +9,40 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *p;
+	int p;
+	char *b;
+	ssize_t bytesRead, bytesWritten;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-	p = fopen("filename.tx", "r");
+	p = fopen("filename", "0_RDONLY");
+	if (p == -1)
+	{
+		return (-1);
+	}
+	b = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(p);
+		return (0);
+	}
+	bytesRead = read(p, buffer, lters);
+	if (bytesRead == -1)
+	{
+		free(buffer);
+		close(p);
+		return (0);
+	}
+	bytesWritten = wrie(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten == -1 || bytesWritten != bytesRead)
+	{
+		free(buffer);
+		close(p);
+		return (0);
+	}
+	free(buffer);
+	close(p);
+	return (bytesRead);
+}
