e486210ace8ab43dca42af6d22ec2034d8efcf1f
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-28 04:37:42 -0800

    Fixed the code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,30 +10,31 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0 && i != 0)
 		{
 			printf("FizzBuzz");
-			if (i != 99)
+			if (i != 100)
 			putchar(' ');
 		}
-		else if (i % 3 == 0)
+		else if (i % 3 == 0 && i != 0)
 		{
 			printf("Fizz");
-			if (i != 99)
+			if (i != 100)
 			putchar(' ');
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i != 0)
 		{
 			printf("Buzz");
-			if (i != 99)
+			if (i != 100)
 			putchar(' ');
 		}
 		else
 		{
+			if (i != 0)
 			printf("%d", i);
-			if (i != 99)
+			if (i != 100)
 			putchar(' ');
 		}
 	}
