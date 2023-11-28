6ea620da676ba23ecfa9e22b75bc5b6bcd7c6199
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-28 11:39:20 -0800

    Fixed the code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,12 @@ void print_rev(char *s)
 {
 	int i, c;
 
+	c = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		c = i;
 	}
-	for (i = 0; c >= 0; c--)
+	for (i = 0; c >= i; c--)
 	{
 		if (s[c] != '\0')
 		_putchar(s[c]);
