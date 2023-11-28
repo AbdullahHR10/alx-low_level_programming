f3237abaa8ffead31f793d1370b69b7505b41fb7
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-28 11:31:04 -0800

    Fixed the code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,20 +11,14 @@ void print_rev(char *s)
 {
 	int i, c;
 
-	if (s == NULL)
-	{
-		_putchar('\n');
-	}
-	else
-	{
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		c = i;
 	}
 	for (i = 0; c >= i; c--)
 	{
+		if (s[c] != NULL)
 		_putchar(s[c]);
 	}
 	_putchar('\n');
-	}
 }
