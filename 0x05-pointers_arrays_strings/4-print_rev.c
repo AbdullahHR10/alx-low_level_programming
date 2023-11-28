2bc61410f8a38a6a713438b02399f48d3f998983
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-28 11:21:21 -0800

    Wrote the code

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse
+ * @s: char
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int i, c;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		c = i;
+	}
+	for (i = 0; c >= i; c--)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}
