85724c97a3363615103478986928ff68b8fb5c4c
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-28 04:26:31 -0800

    Fixed the code

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			if (i != 99)
+			putchar(' ');
+		}
+		if (i % 5 == 0)
+		{
+			printf("Buzz");
+			if (i != 99)
+			putchar(' ');
+		}
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			if (i != 99)
+			putchar(' ');
+		}
+	}
+}
