9b5cf7457ec81c36d595b2dca8a117b57012e183
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-29 04:20:26 -0800

    Fixed the code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,6 +15,10 @@ char *_strncpy(char *dest, char *src, int n)
 	if (src[0] == '\0')
 	{
 		dest[0] = '\0';
+		for (i = 1; i < n; i++)
+		{
+			dest[i] = '\0';
+		}
 		return (dest);
 	}
 	for (i = 0; src[i] != '\0' && n > 0; i++, n--)
