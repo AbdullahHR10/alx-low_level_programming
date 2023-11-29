adfdd10980e81925d998f43110a940d7b4909b74
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-29 04:01:11 -0800

    Wrote the code

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	if (src[0] = '\0')
+	{
+		return (0);
+	}
+	for (i = 0; src[i] != '\0' && n > 0; i++, n--)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
