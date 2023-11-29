fb65a4dfbc2993564ef9f33f9b2e1a5e66a6170c
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-29 10:09:50 -0800

    Wrote the code

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: char
+ * @s2: char
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, c;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	}
+	if (i > c)
+	{
+		return (-i);
+	}
+	if (c > i)
+	{
+		return (-c);
+	}
+	else
+	{
+		return (0);
+	}
+}
