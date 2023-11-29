a61ef7d4baf145ab854ff98bd8c464b2646ad3c8
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-28 23:53:58 -0800

    Wrote the code

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: string
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int i, n;
+
+	if (str[0] = '\0')
+	{
+		return;
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	n = (i - 2) / 2;
+	for (n = n + 1; n < i; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
