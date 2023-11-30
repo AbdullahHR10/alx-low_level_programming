eab18ea67f76b97eb5c683ff70caa38a759e2900
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-29 21:48:19 -0800

    To be fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,24 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, c;
+        int i, c;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-	for (c = 0; s2[c] != '\0'; c++)
-	{
-	}
-	if (i > c)
-	{
-		return (1);
-	}
-	if (c > i)
-	{
-		return (-1);
-	}
-	else
-	{
-		return (0);
-	}
+        for (i = 0; s1[i] != '\0'; i++)
+        {
+        }
+        for (c = 0; s2[c] != '\0'; c++)
+        {
+        }
+        if (*s1 > *s2)
+        {
+            return (1);
+        }
+        if (*s2 > *s1)
+        {
+            return (-1);
+        }
+        else
+        {
+            return (0);
+        }
 }
