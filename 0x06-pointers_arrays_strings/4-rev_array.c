158334bfc107748dcea77cb8e7d91b2dd3341e13
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-29 22:18:37 -0800

    Wrote the code

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses an array
+ * @a: array
+ * @n: size of array
+ * Return: Nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	if (n == 0)
+	{
+		return;
+	}
+	for (i = 0; i < n; i++, n--)
+	{
+		temp = a[n - 1];
+		a[n] = a[i];
+		a[i] = temp;
+	}
+}
