df5380580470eaaf68b5dd9a18108f3fae71dcb5
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-06 05:39:56 -0800

    Wrote the code

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2 dimensional grid by alloc_grid function
+ * @grid: grid
+ * @height: height
+ * Return: Nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	free(grid);
+}
