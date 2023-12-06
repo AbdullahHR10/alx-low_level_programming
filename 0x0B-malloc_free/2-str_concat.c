04fb80e1c41680b7068a346e7766483e3c353109
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-06 05:57:20 -0800

    Fixed the code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *ptr;
 
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
 	ptr = (char *)malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	if (ptr == NULL)
 	{
