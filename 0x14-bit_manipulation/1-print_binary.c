e5edea7beff37c86755e4363669cb356718b9f8f
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-14 01:19:38 -0800

    Reveresed last change and added an if condition

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,7 +11,7 @@ void print_binary(unsigned long int n)
 	int i, bit, c;
 
 	c = 0;
-	for (i = sizeof(unsigned long int) * 8 - 1; i > 0; i--)
+	for (i = sizeof(unsigned long int) * 8 - 1; i >= 0; i--)
 	{
 		bit = (n >> i) & 1;
 		if (bit == 1)
@@ -22,5 +22,9 @@ void print_binary(unsigned long int n)
 		{
 			_putchar(bit + '0');
 		}
+	}
+	if (n == 0)
+	{
+		_putchar('0');
 	}
 }
