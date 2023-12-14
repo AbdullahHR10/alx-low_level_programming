85c8a95bb99452b0a704960e94ce26ff797b3730
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-14 01:11:20 -0800

    Wrote the code and debugged it

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: number
+ * Return: Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, bit, c;
+
+	c = 0;
+	for (i = sizeof(int) * 8 - 1; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
+		if (bit == 1)
+		{
+			c = 1;
+		}
+		if (c == 1)
+		{
+			_putchar(bit + '0');
+		}
+	}
+}
