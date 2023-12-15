614f78778f3fcaadbdbeb4c99ba56e2337addb27
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-14 23:56:23 -0800

    Fixed a bug

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,18 +8,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum, value, value2;
+	int sum, value, value2;
 
 	if (argc != 3)
 	{
 		printf("ERROR\n");
 		return (1);
 	}
-	for (i = 1; i < argc - 1; i++)
-	{
-		value = atoi(argv[1]);
-		value2 = atoi(argv[2]);
-	}
+	value = atoi(argv[1]);
+	value2 = atoi(argv[2]);
 	sum = value * value2;
 	printf("%d\n", sum);
 	return (sum);
