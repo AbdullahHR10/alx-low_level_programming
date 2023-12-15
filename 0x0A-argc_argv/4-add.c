1badba07f3fa5f9d0ef42769b585153b19996668
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-15 01:30:38 -0800

    Added an error condition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,15 +8,23 @@
  */
 int main(int argc, char *argv[])
 {
-        int i, sum = 0;
+	int i, sum = 0;
 
-        if (argc == 1)
+	if (argc == 1)
 	{
 		printf("0\n");
 	}
 	for (i = 1; i < argc; i++)
 	{
-		sum = sum + atoi(argv[i]);
+		if (atoi(argv[i]) > -100000 || atoi(argv[i]) < 100000)
+		{
+			sum = sum + atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
         printf("%d\n", sum);
         return (0);
