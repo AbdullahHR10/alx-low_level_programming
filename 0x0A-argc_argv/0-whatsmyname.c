280bbcc6baae5066c0e859d0b2ff53e9dc39b4e1
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-14 23:04:41 -0800

    Wrote the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of strings
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+}
