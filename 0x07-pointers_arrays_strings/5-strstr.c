151123210ef3af1438950d5837e9f8732541340a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-15 23:15:50 -0800

    Wrote a starter code

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: the main string
+ * @needle: substring
+ * Return: a pointer to the substring or NULL if substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			needle[j] = haystack[i];
+		}
+	}
+	return (needle);
+}
