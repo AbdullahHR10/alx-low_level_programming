2d1ca3a22bff589dfb20de84e3605cb65517d81a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-15 22:43:13 -0800

    Wrote a starter code

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: pointer to the located char
+ * @c: char to be located
+ * Return: a pointer to the char (s), or NULL if char was not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
