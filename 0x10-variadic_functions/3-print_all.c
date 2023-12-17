fb6e2f22a24150f43a2f31382aeb0690abc9d021
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-17 00:13:48 -0800

    Fixed the code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -17,7 +17,7 @@ void print_all(const char * const format, ...)
 	char *s;
 	const char *ptr = format;
 
-	(format == NULL) ? printf("(nil)\n") : (void)0;
+	(void)(format != NULL && printf("(nil)\n"));
 	va_start(args, format);
 	while (*ptr != '\0')
 	{
