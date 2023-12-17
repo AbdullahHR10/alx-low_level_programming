90ea4174cb05d220c3f422f57bc3d2dd6fda05cb
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-16 22:29:30 -0800

    Wrote the code

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "variadic_functions.h"
 
 /**
  * print_numbers - Print numbers
@@ -8,3 +8,21 @@
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int c;
+	va_list args;
+
+	va_start (args, n);
+	for (i = 0; i < n; i++)
+	{
+		c = va_arg(args, int);
+		printf("%d", c);
+		if (separator != NULL)
+		{
+			printf("%s", seprator);
+		}
+	}
+	_putchar('\n');
+	va_end (args);
+}
