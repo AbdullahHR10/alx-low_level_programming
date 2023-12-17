bc0a290d2cc10dcca5bbc42d9a05e4aff5741249
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-16 22:12:03 -0800

    Added comments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers
+ * @separator: string to be printed between numbers
+ * @n: number of arguments
+ * Return: Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
