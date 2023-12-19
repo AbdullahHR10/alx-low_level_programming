cfe11d7968def6a296e88cea08ac24f477a2748b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-19 01:36:37 -0800

    Fixed some syntax errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,14 +11,14 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	char *strlength;
-	ssize_t = write_result;
+	size_t strlength;
+	ssize_t write_result;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	if (text_content == NULL || text_content[0] = '\0')
+	if (text_content == NULL || text_content[0] == '\0')
 	{
 		return (1);
 	}
@@ -35,5 +35,5 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(fd);
-	return (-1);
+	return (1);
 }
