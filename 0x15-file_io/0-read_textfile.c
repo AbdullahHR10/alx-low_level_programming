c824cd34ba0d6d06b022fc398019d75d60466947
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-19 00:17:22 -0800

    Fixed the code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -29,6 +29,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	bytesRead = fread(memory, sizeof(char), letters, fp);
 	bytesWritten = fwrite(memory, sizeof(char), bytesRead, stdout);
+	if (bytesWritten != bytesRead)
+	{
+		fclose(fp);
+		free(memory);
+		return (0);
+	}
 	fclose(fp);
 	memory[bytesRead] = '\0';
 	free(memory);
