ddb5caa427cae8d403275a8a9d75d97d34e2e304
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-19 01:17:50 -0800

    Wrote the code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,26 +10,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, strlength;
 	ssize_t write_result;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	if (text_content != NULL)
+	strlength = strlen(text_content);
+	write_result = write(fd, text_content, strlength);
+	if (write_result == -1)
 	{
-		write_result = write(fd, text_content, strlen(text_content));
-		if (write_result == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (1);
