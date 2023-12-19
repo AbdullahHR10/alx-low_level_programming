16bf09ce6d189af2f691d217ddd4746d08dbfe56
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-19 01:33:35 -0800

    Wrote the code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,3 +9,31 @@
  * 1 if the file exists, -1 if not
  */
 int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	char *strlength;
+	ssize_t = write_result;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL || text_content[0] = '\0')
+	{
+		return (1);
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	strlength = strlen(text_content);
+	write_result = write(fd, text_content, strlength);
+	if (write_result == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (-1);
+}
