ed3ba9116b5f3229746a56bc5ed26cd27e171446
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-22 21:37:34 -0800

    Added stdlib to main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
 
+#include <stdlib.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
