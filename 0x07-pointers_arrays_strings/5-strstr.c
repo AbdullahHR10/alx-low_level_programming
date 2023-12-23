52a9b968308095e352de492bf9098349506a4909
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-12-22 21:55:46 -0800

    Fixed the code

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	const char *h, *n;
 
-	if (haystack == NULL || needle == NULL || *needle == '\0')
+	if (haystack == NULL || needle == NULL)
 	{
 		return (NULL);
 	}
