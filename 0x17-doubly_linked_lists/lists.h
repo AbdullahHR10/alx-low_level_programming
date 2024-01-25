d04ddc2dad084cb0f26c9c1b8bd2752bef33084a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-01-24 22:20:04 -0800

    Added guards

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_HEADER
+#define LISTS_HEADER
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -27,3 +30,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+#endif
