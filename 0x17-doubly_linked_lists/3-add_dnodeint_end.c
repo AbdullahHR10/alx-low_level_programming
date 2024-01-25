f145320573c06021231ee172bbcccc63ad98fdbf
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-01-25 11:02:11 -0800

    Fixed betty style

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,24 +2,24 @@
 /**
  * add_dnodeint_end - Adds a new node at the end of a dlistint_t list
  * @head: element to be added
- * @@n: connst int
+ * @n: connst int
  * Return: The address of the new element, or NULL if it failed
  */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node, *last_node;
-	
+
 	if (head == NULL)
 		return (NULL);
-	
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
-	
-	if (*head == NULL) 
+
+	if (*head == NULL)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
@@ -27,7 +27,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		last_node = *head;
-		while (last_node->next != NULL) {
+		while (last_node->next != NULL)
+		{
 			last_node = last_node->next;
 		}
 		last_node->next = new_node;
