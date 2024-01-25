316ea2786801c9f65b1e4980fde72de36150dd06
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-01-25 01:36:35 -0800

    Changed the comments

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_dnodeint - Adds a new node at the beginning of a dlistint_t list
  * @head: new node to be added
- * @n: int
+ * @n: const int
  * Return: the address of the new element, or NULL if it failed
  */
 
