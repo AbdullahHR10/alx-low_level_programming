303ad1a88fc495d16e10a381dbcb3797cbf88c5a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-01-24 21:58:47 -0800

    Wrote the code

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list
+ * @h: list
+ * Return: Number of elements
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
