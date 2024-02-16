14504226c131163ccf51543209c7a50069733f17
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-16 10:11:01 -0800

    Wrote the code

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,32 @@
+#include "hash.tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table to add the key/value pair to.
+ * @key: The key to add.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if the addition succeeds, 0 otherwise.
+ *         In case of collision, adds the new node at the beginning of the list.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return (0);
+	}
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	new_node = create_hash_node(key, value);
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
