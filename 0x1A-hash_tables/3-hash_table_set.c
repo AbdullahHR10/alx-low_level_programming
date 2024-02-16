62d5f5fdf81207ad5327093a51b3b714f293ce20
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-16 10:24:58 -0800

    Wrote the code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	index = hash_djb2((const unsigned char *)key) % ht->size;
-	new_node = create_hash_node(key, value);
+	new_node = (key, value);
 	if (new_node == NULL)
 	{
 		return (0);
