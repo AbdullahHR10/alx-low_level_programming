cb1f4c62f1bf19143f167f8b9773c4522b636020
AbdullahHR10 <AbdullahH.ragab10@gmail.com>
2024-03-02 07:52:46 -0800

    Wrote the program

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,15 @@ int main(void)
 {
 	int i;
 	char alpha[27] = "abcdefghijklmnopqrstuvwxyz";
+	char alphaB[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	for (i = 0; alpha[i] != '\0'; i++)
 	{
 		putchar(alpha[i]);
+	}
+	for (i = 0; alphaB[i] != '\0'; i++)
+	{
+		putchar(alphaB[i]);
 	}
 	putchar('\n');
 	return (0);
