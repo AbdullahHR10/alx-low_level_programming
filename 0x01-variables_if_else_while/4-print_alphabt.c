7de2560a45bd37c0b06f5239c5de9e5b0f0fe096
AbdullahHR10 <AbdullahH.ragab10@gmail.com>
2024-03-02 08:47:59 -0800

    Wrote the program

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	char alpha[25] = "abcdfghijklmnoprstuvwxyz";
+
+	for (i = 0; alpha[i] != '\0'; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
