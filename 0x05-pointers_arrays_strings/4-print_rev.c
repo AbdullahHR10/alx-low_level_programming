6805cc4cfdb39851c43ac78eced28744b5b9e7a6
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-10 15:50:49 -0700

    Fixed the function

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,27 @@
 #include "main.h"
 
-int _strlen(char *s);
+/**
+ * _strlen - Returns the length of a string
+ * @s: The string to calculate the length of
+ *
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+        int i = 0;
+
+        while (s[i] != '\0')
+                i++;
+        return (i);
+}
+
 /**
  * print_rev - Prints a string, in reverse
  * @s: String to be printed
  *
  * Return: Nothing
  */
-
 void print_rev(char *s)
 {
 	int i = _strlen(s);
