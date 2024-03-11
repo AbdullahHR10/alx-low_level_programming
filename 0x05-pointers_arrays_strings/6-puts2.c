6f76bd40e0f08fb9d9224e8251ffabd349b5623f
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-10 19:15:14 -0700

    Fixed the function

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i + 1] != '\0';)
+	for (i = 0; str[i] != '\0', str[i + 1] != '\0'; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
 	_putchar('\n');
 }
