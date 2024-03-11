db8dc4edd4d4f8982f72800f4d7a3d7671bbdbd1
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-10 18:57:24 -0700

    Wrote the function

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: String to be printed
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0';)
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+}
